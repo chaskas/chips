410079f17ec6995e8c26cca91244a37c0df6605c
Rodrigo Campos <chaskas@MacBook-Pro-de-Rodrigo.local>
2018-10-22 21:02:53 -0300

    added functionality for push chips and delete.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Chip } from './chip';
+import { ChipsDataService } from './chips-data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'chips';
+
+  constructor (
+    private chipsDataService: ChipsDataService
+  ) { }
+
+  newChip: Chip = new Chip();
+
+  addChip() {
+    this.chipsDataService.addChip(this.newChip);
+    this.newChip = new Chip();
+  }
+
+  removeChip(chip) {
+    this.chipsDataService.deleteChipById(chip.id);
+  }
+
+  get chips() {
+    return this.chipsDataService.getAllChips();
+  }
+
+
 }
