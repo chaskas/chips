0536036a1f762a3fe3e89c4b597b3c51701c2f88
Rodrigo Campos <chaskas@MacBook-Pro-de-Rodrigo.local>
2018-10-22 21:02:06 -0300

    FormsModule and ChipsDataServices provided

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ChipsDataService } from './chips-data.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ChipsDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
